2be00aa7be35cf24f8510c6f1a83db4e05c48825
Jonathas Lima <alexandrejonathas@gmail.com>
2024-11-27 16:41:38 -0300

    feat: add nome e test para endpoint lista de generos

--- genres/tests.py
+++ genres/tests.py
@@ -1,3 +1,16 @@
+from django.urls import resolve, reverse
+
 from django.test import TestCase
 
-# Create your tests here.
+import json
+
+class GenreApiTest(TestCase):
+
+    def test_genre_api_list_endpoint(self):
+
+        response = self.client.get(reverse('genres:list'))
+
+        genres = json.loads(response.content)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(int(genres['id']), 1)
--- genres/urls.py
+++ genres/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from genres import views
 
+app_name = 'genres'
+
 urlpatterns = [
-    path('api/genres', views.index),
+    path('api/genres', views.index, name='list'),
 ]
