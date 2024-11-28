c55736e93dc27d79bf2961ab0317dd4cebe91ef3
Jonathas Lima <alexandrejonathas@gmail.com>
2024-11-28 14:44:32 -0300

    feat: criando modelo de atores

--- actors/models.py
+++ actors/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+NATIONALITY_CHOICES = (
+    ('ARG', 'Argentina'),
+    ('BR', 'Brasil'),
+    ('CRO', 'Croacia'),
+    ('DIN', 'Dinamarca'),
+    ('ESP', 'Espanha'),
+    ('FRA', 'França'),
+    ('GEO', 'Georgia'),
+    ('HOL', 'Holanda'),
+    ('ITA', 'Italia'),
+    ('JAP', 'Japao'),
+)
+
+
+class Actor(models.Model):
+
+    name = models.CharField(max_length=100)
+    birthday = models.DateField(null=True, blank=False)
+    nationality = models.CharField(
+        max_length=100, 
+        choices=NATIONALITY_CHOICES,
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
