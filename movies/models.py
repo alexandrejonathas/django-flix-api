12bb4149233f2d42ec56538507625882ea429b35
Jonathas Lima <alexandrejonathas@gmail.com>
2024-11-29 08:18:50 -0300

    feat: corrigindo on_delete

--- movies/models.py
+++ movies/models.py
@@ -7,7 +7,7 @@ class Movie(models.Model):
     
     title = models.CharField(max_length=100)
     genre = models.ForeignKey(
-        Genre, on_delete=models.SET_NULL, related_name='movies'
+        Genre, on_delete=models.PROTECT, related_name='movies'
     )
     release_date = models.DateField(null=True, blank=True)
     actors = models.ManyToManyField(Actor, related_name='movies')
