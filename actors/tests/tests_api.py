bb46b4c4d867e8827624dcbeae2e35684be40b87
Jonathas Lima <alexandrejonathas@gmail.com>
2024-12-10 13:53:01 -0300

    chore: corrigindo tests de atores

--- actors/tests/tests_api.py
+++ actors/tests/tests_api.py
@@ -7,9 +7,23 @@ from actors.models import Actor
 
 class ActorApiTest(ActorBaseTest):
 
+    def setUp(self):
+        self.userdata = {'username': 'user', 'password': 'password'}
+        
+        self.user = self.make_user(
+            username=self.userdata.get('username'),
+            password=self.userdata.get('password')
+        )
+
     def test_api_resource_actor_method_not_allowed(self):
 
-        response = self.client.patch(reverse('actors:actors_create_list'))
+        self.make_user_permissions(user=self.user, model=Actor, perms=['view_actor', 'change_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
+        response = self.client.patch(
+            reverse('actors:actors_create_list'),
+            headers={'Authorization': f'Bearer {token}'}
+        )
 
         self.assertEqual(response.status_code, 405)
 
@@ -18,7 +32,13 @@ class ActorApiTest(ActorBaseTest):
         self.make_actor(name='Silvester Stalone')
         self.make_actor(name='Sharon Stone')
 
-        response = self.client.get(reverse('actors:actors_create_list'))
+        self.make_user_permissions(user=self.user, model=Actor, perms=['view_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
+        response = self.client.get(
+            reverse('actors:actors_create_list'),
+            headers={'Authorization': f'Bearer {token}'}
+        )
 
         actors = json.loads(response.content.decode('utf-8'))
 
@@ -27,10 +47,14 @@ class ActorApiTest(ActorBaseTest):
 
     def test_api_resource_actor_create(self):
 
+        self.make_user_permissions(user=self.user, model=Actor, perms=['add_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
         response = self.client.post(
             reverse('actors:actors_create_list'),
             data=json.dumps({'name': 'Silvester Stalone'}),
-            content_type='application/json'
+            content_type='application/json',
+            headers={'Authorization': f'Bearer {token}'}
         )
 
         actor = json.loads(response.content.decode('utf-8'))
@@ -44,8 +68,12 @@ class ActorApiTest(ActorBaseTest):
         self.make_actor(name='Silvester Stalone')
         self.make_actor(name='Sharon Stone')
 
+        self.make_user_permissions(user=self.user, model=Actor, perms=['view_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
         response = self.client.get(
             reverse('actors:actors_retrieve_update_delete', kwargs={'pk': 3}),
+            headers={'Authorization': f'Bearer {token}'}
         )
 
         self.assertEqual(response.status_code, 404)                  
@@ -55,8 +83,12 @@ class ActorApiTest(ActorBaseTest):
         self.make_actor(name='Silvester Stalone')
         self.make_actor(name='Sharon Stone')
 
+        self.make_user_permissions(user=self.user, model=Actor, perms=['view_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
         response = self.client.get(
             reverse('actors:actors_retrieve_update_delete', kwargs={'pk': 1}),
+            headers={'Authorization': f'Bearer {token}'}
         )
 
         actor = json.loads(response.content.decode('utf-8'))
@@ -70,10 +102,14 @@ class ActorApiTest(ActorBaseTest):
         self.make_actor(name='Silvester Stalone')
         self.make_actor(name='Sharon Stone')
 
+        self.make_user_permissions(user=self.user, model=Actor, perms=['change_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
         response = self.client.put(
             reverse('actors:actors_retrieve_update_delete', kwargs={'pk': 1}),
             data=json.dumps({'name': 'Jack Chan'}),
-            content_type='application/json'
+            content_type='application/json',
+            headers={'Authorization': f'Bearer {token}'}
         )
 
         actor = json.loads(response.content.decode('utf-8'))
@@ -87,10 +123,14 @@ class ActorApiTest(ActorBaseTest):
         self.make_actor(name='Silvester Stalone')
         self.make_actor(name='Sharon Stone')
 
+        self.make_user_permissions(user=self.user, model=Actor, perms=['change_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
         response = self.client.put(
             reverse('actors:actors_retrieve_update_delete', kwargs={'pk': 3}),
             data=json.dumps({'name': 'Jack Chan'}),
-            content_type='application/json'
+            content_type='application/json',
+            headers={'Authorization': f'Bearer {token}'}
         )
 
         self.assertEqual(response.status_code, 404)
@@ -100,8 +140,12 @@ class ActorApiTest(ActorBaseTest):
         self.make_actor(name='Silvester Stalone')
         self.make_actor(name='Sharon Stone')
 
+        self.make_user_permissions(user=self.user, model=Actor, perms=['delete_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
         response = self.client.delete(
             reverse('actors:actors_retrieve_update_delete', kwargs={'pk': 1}),
+            headers={'Authorization': f'Bearer {token}'}
         )
 
         self.assertEqual(response.status_code, 204)
@@ -111,8 +155,12 @@ class ActorApiTest(ActorBaseTest):
         self.make_actor(name='Silvester Stalone')
         self.make_actor(name='Sharon Stone')
 
+        self.make_user_permissions(user=self.user, model=Actor, perms=['delete_actor'])  # noqa
+        token = self.get_jwt_token(userdata=self.userdata)
+
         response = self.client.delete(
             reverse('actors:actors_retrieve_update_delete', kwargs={'pk': 3}),
+            headers={'Authorization': f'Bearer {token}'}
         )
 
         self.assertEqual(response.status_code, 404)  
